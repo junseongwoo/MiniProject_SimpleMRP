7674268632cd16d1b15f67d3ef4e72d49940f24e
junseongwoo <wnstjd4852@gmail.com>
2021-06-23 17:32:20 +0900

    0623 컬러센서 DB 연동 확인

---
+++ MRPApp/DeviceSubApp/FrmMain.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace DeviceSubApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtConnectionString = new System.Windows.Forms.TextBox();
+            this.TxtClientID = new System.Windows.Forms.TextBox();
+            this.TxtSubscriptionTopic = new System.Windows.Forms.TextBox();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.RtbSubscr = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.LblResult = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connenction String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Subscription Topic";
+            // 
+            // TxtConnectionString
+            // 
+            this.TxtConnectionString.Location = new System.Drawing.Point(180, 41);
+            this.TxtConnectionString.Name = "TxtConnectionString";
+            this.TxtConnectionString.Size = new System.Drawing.Size(503, 21);
+            this.TxtConnectionString.TabIndex = 1;
+            this.TxtConnectionString.Text = "210.119.12.215";
+            // 
+            // TxtClientID
+            // 
+            this.TxtClientID.Location = new System.Drawing.Point(180, 78);
+            this.TxtClientID.Name = "TxtClientID";
+            this.TxtClientID.Size = new System.Drawing.Size(503, 21);
+            this.TxtClientID.TabIndex = 1;
+            this.TxtClientID.Text = "SUBSCR01";
+            // 
+            // TxtSubscriptionTopic
+            // 
+            this.TxtSubscriptionTopic.Location = new System.Drawing.Point(180, 115);
+            this.TxtSubscriptionTopic.Name = "TxtSubscriptionTopic";
+            this.TxtSubscriptionTopic.Size = new System.Drawing.Size(503, 21);
+            this.TxtSubscriptionTopic.TabIndex = 1;
+            this.TxtSubscriptionTopic.Text = "factory1/machine1/data/";
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(984, 103);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(85, 43);
+            this.BtnConnect.TabIndex = 2;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Location = new System.Drawing.Point(1075, 103);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(86, 43);
+            this.BtnDisconnect.TabIndex = 2;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // RtbSubscr
+            // 
+            this.RtbSubscr.Location = new System.Drawing.Point(12, 159);
+            this.RtbSubscr.Name = "RtbSubscr";
+            this.RtbSubscr.Size = new System.Drawing.Size(1149, 477);
+            this.RtbSubscr.TabIndex = 3;
+            this.RtbSubscr.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LblResult});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 639);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1184, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // LblResult
+            // 
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(14, 17);
+            this.LblResult.Text = "0";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.RtbSubscr);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.TxtSubscriptionTopic);
+            this.Controls.Add(this.TxtClientID);
+            this.Controls.Add(this.TxtConnectionString);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "IoT Device Subscriber";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtConnectionString;
+        private System.Windows.Forms.TextBox TxtClientID;
+        private System.Windows.Forms.TextBox TxtSubscriptionTopic;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.RichTextBox RtbSubscr;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel LblResult;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
---
+++ MRPApp/DeviceSubApp/FrmMain.cs
@@ -0,0 +1,187 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace DeviceSubApp
+{
+    public partial class FrmMain : Form
+    {
+        MqttClient client;
+        string connectionString; // DB 연결 문자열 | MQTT Broker address
+        ulong lineCount;
+        delegate void UpdateTextCallback(string message);
+
+        Stopwatch sw = new Stopwatch();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitializeAllData();
+        }
+
+        private void InitializeAllData()
+        {
+            connectionString = "Data Source=" + TxtConnectionString.Text + 
+                ";Initial Catalog=MRP;" +
+                "Persist Security Info=True;" +
+                "User ID=sa;" +
+                "Password=mssql_p@ssw0rd!";
+            lineCount = 0;
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
+            IPAddress brokerAddress;
+
+            try
+            {
+                brokerAddress = IPAddress.Parse(TxtConnectionString.Text);
+                client = new MqttClient(brokerAddress);
+                client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            Timer.Enabled = true;
+            Timer.Interval = 1000; // 1sec
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            LblResult.Text = sw.Elapsed.Seconds.ToString();
+            if (sw.Elapsed.Seconds >= 3)
+            {
+                sw.Stop();
+                sw.Reset();
+
+                UpdateText("처리!!");
+                PrcCorrectDataToDB();
+            }
+        }
+
+        private void PrcCorrectDataToDB()
+        {
+            if(iotData.Count>0)
+            {
+                var correctData = iotData[iotData.Count - 1];
+
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    var prcResult = correctData["PRC_MSG"] == "OK" ? 1 : 0;
+                    string strUpQry = $"UPDATE Process_DEV " +
+                                      $"   SET PrcEndTime = '{DateTime.Now.ToString("HH:mm:ss")}' " +
+                                      $"     , PrcResult = '{prcResult}' " +
+                                      $"     , ModDate = '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}' " +
+                                      $"     , ModID = '{"SYS"}' " +
+                                      $"  WHERE PrcIdx = " + 
+                                      $" (SELECT TOP 1 PrcIdx FROM Process_DEV ORDER BY PrcIdx DESC)";
+
+                    try
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand(strUpQry, conn);
+                        if (cmd.ExecuteNonQuery() == 1)
+                            UpdateText("[DB] 센싱값 Update 성공");
+                        else
+                            UpdateText("[DB] 센싱값 Update 실패");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        UpdateText($">>>> DB ERROR!! : {ex.Message}");
+                    }
+                }
+
+                
+            }
+
+            iotData.Clear(); // 데이터 모두 삭제
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            try
+            {
+                var message = Encoding.UTF8.GetString(e.Message);
+                UpdateText($">>>>>> 받은 메시지 : {message}");
+                // message(json) > C# 
+                var currentData = JsonConvert.DeserializeObject
+                    <Dictionary<string, string>>(message);
+                PrcInputDataToList(currentData);
+
+                sw.Stop();
+                sw.Reset();
+                sw.Start();
+
+            }
+            catch (Exception ex)
+            {
+                UpdateText($">>>> ERROR!! : {ex.Message}");
+            }
+        }
+
+        List<Dictionary<string, string>> iotData = new List<Dictionary<string, string>>();
+        
+        // 라즈베리에서 들어온 메세지를 필터링해서 처리한 메서드 
+        private void PrcInputDataToList(Dictionary<string, string> currentData)
+        {
+            if (currentData["PRC_MSG"] == "OK" || currentData["PRC_MSG"] == "FAIL")
+            {
+                iotData.Add(currentData);
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            client.Connect(TxtClientID.Text);
+            UpdateText(">>>>>> Client Connected");
+            
+            client.Subscribe(new string[] { TxtSubscriptionTopic.Text },
+                new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+        
+            UpdateText(">>>>>> Subscribing to : " 
+                + TxtSubscriptionTopic.Text );
+
+            BtnConnect.Enabled = false;
+            BtnDisconnect.Enabled = true;
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+            UpdateText(">>>>> Client disconnected!!");
+
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
+        }
+
+        private void UpdateText(string message)
+        {
+            if (RtbSubscr.InvokeRequired)
+            {
+                UpdateTextCallback callback = new UpdateTextCallback(UpdateText);
+                this.Invoke(callback, new object[] { message });
+            }
+            else
+            {
+                lineCount++; 
+                RtbSubscr.AppendText($"{lineCount} : {message}\n");
+                RtbSubscr.ScrollToCaret();
+            }
+        }
+    }
+}
